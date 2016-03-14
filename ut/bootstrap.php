9e9781f1e2118ed2a73a493bfacdfe42c7965377
Minhao Zhang <minhao.zhang@qq.com>
2016-03-14 23:25:14 +0800

    * enable catching fatal error in UT

--- ut/bootstrap.php
+++ ut/bootstrap.php
@@ -7,6 +7,7 @@
  */
 use Composer\Autoload\ClassLoader;
 use Oasis\Mlib\Logging\LocalFileHandler;
+use Symfony\Component\Debug\Debug;
 
 //require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -14,6 +15,7 @@ use Oasis\Mlib\Logging\LocalFileHandler;
 $loader = require_once __DIR__ . "/../vendor/autoload.php";
 $loader->addPsr4('Oasis\\Mlib\\Http\\Ut\\', __DIR__ . "/Helpers");
 
-error_reporting(E_ALL ^ ~E_NOTICE);
-
+//error_reporting(E_ALL ^ ~E_NOTICE);
+Debug::enable(E_ALL ^ ~E_NOTICE);
 (new LocalFileHandler('/tmp'))->install();
+

9e2ce239ea529b366e620f2d4ebb204b43f30643
Minhao Zhang <minhao.zhang@qq.com>
2016-03-28 16:42:59 +0800

    + twig config test cases

---
+++ ut/TwigServiceProviderConfigurationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2016-03-25
+ * Time: 11:53
+ */
+class TwigServiceProviderConfigurationTest extends TwigServiceProviderTest
+{
+    
+    /**
+     * Creates the application.
+     *
+     * @return HttpKernelInterface
+     */
+    public function createApplication()
+    {
+        return require __DIR__ . "/app.twig2.php";
+    }
+
+}
